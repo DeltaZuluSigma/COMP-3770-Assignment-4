87eca90ed39e25ae2df14cf985d72dd339d35b5a
mattpotter2442 <73261257+mattpotter2442@users.noreply.github.com>
2020-11-16 21:20:11 -0500

    stuff

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    // text fields
+    public Text time;
+    public Text popped;
+    public Text missed;
+    public Button end;
+    public Text endText;
+    // associated variables
+    private float t;
+    private int p;
+    private int m;
+    //spawn point array
+    public SpawnPoint[] spawnPoints = new SpawnPoint[7];
+    //temp for scores
+    private int n;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = 0;
+        m = 0;
+        end.interactable = false;
+        endText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        updateNumbers();
+        time.text = "" + System.Math.Round(Time.time, 2);
+        popped.text = "" + p;
+        missed.text = "" + m;
+
+        if (m >= 10)
+            endGame();
+    }
+
+    public void updateNumbers()
+    {
+        p = spawnPoints[0].nPopped + spawnPoints[1].nPopped + spawnPoints[2].nPopped + spawnPoints[3].nPopped + spawnPoints[4].nPopped + spawnPoints[5].nPopped + spawnPoints[6].nPopped;
+        m = spawnPoints[0].nMissed + spawnPoints[1].nMissed + spawnPoints[2].nMissed + spawnPoints[3].nMissed + spawnPoints[4].nMissed + spawnPoints[5].nMissed + spawnPoints[6].nMissed;
+    }
+
+     public void endGame()
+     {
+        for(int i = 0; i <= spawnPoints.Length - 1; i++)
+            spawnPoints[i].disabled = true;
+        
+
+         PlayerPrefs.SetInt("bubbleScore", p);
+         PlayerPrefs.Save();
+         endText.text = "Main Menu";
+         end.interactable = true;
+    }
+
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public Bubble bubble;
+    private Bubble clone;
+    //spawn position
+    private Vector3 spawnPos;
+    // to determine which directions bubbles will spawn
+    public bool down;
+    public bool right;
+    public bool left;
+    public bool up;
+    // counters for missed and popped from this spawner
+    public int nPopped;
+    public int nMissed;
+    // speed var
+    public float speed;
+    //stop spawns
+    public bool disabled;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        disabled = false;
+        spawnBubble();
+
+        nPopped = 0;
+        nMissed = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (disabled == false)
+        {
+            if (clone.isPopped == true)
+            {
+                nPopped += 1;
+                Destroy(this.clone.gameObject);
+                spawnBubble();
+                clone.isPopped = false;
+            }
+            else moveBubble();
+
+            if (clone.isMissed == true)
+            {
+                nMissed += 1;
+                Destroy(this.clone.gameObject);
+                spawnBubble();
+                clone.isMissed = false;
+            }
+            else moveBubble();
+        }
+    }
+    public void spawnBubble()
+    {
+        spawnPos = this.transform.position;
+        clone = Instantiate(bubble);
+        clone.transform.position = spawnPos;
+    }
+
+    public void moveBubble()
+    {
+        if (down == true)
+            clone.transform.position += Vector3.down * speed * Time.deltaTime;
+        if (up == true)
+            clone.transform.position += Vector3.up * speed * Time.deltaTime;
+        if (right == true)
+            clone.transform.position += Vector3.right * speed * Time.deltaTime;
+        if (left == true)
+            clone.transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
